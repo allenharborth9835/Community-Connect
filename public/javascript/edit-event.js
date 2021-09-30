5b1df353d745747e660b496f50e51b2bb75c0df5
Harborth, Allen <allenharborth9835@gmail.com>
2021-09-29 21:14:51 -0500

    fix edit-event function

--- public/javascript/edit-event.js
+++ public/javascript/edit-event.js
@@ -1,7 +1,10 @@
 async function editFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const event_name = document.querySelector('input[name="event_name"]').value;
+  const location = document.querySelector('input[name="location"]').value;
+  const zip = document.querySelector('input[name="zip"]').value;
+  const event_category = document.querySelector('input[name="event_category"]').value;
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
