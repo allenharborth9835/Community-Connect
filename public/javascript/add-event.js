8b027dfb500f7cfaa6a978d1630f7b2254271cc3
Harborth, Allen <allenharborth9835@gmail.com>
2021-09-29 21:04:32 -0500

    create basic Javascript web functions

---
+++ public/javascript/add-event.js
@@ -0,0 +1,28 @@
+async function newFormHandler(event){
+  event.preventDefault();
+
+  const event_name = document.querySelector('input[name="event_name"]').value;
+  const location = document.querySelector('input[name="location"]').value;
+  const zip = document.querySelector('input[name="zip"]').value;
+  const event_category = document.querySelector('input[name="event_category"]').value;
+
+
+  const response = await fetch(`/api/events`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      post_url
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
